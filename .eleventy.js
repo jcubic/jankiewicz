c9a12c7e320e85ccfcfec3fc9955e3cfa3643b27
Jakub Jankiewicz <jcubic@onet.pl>
2021-07-24 09:48:39 +0200

    add handling of code snippets in posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.setTemplateFormats([
         "md"
     ]);
+    eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPassthroughCopy({ "static": "." });
 };
