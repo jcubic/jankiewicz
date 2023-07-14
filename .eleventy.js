79a975059e2b3145bd5a171e7fcf55904a706212
Jakub Jankiewicz <jcubic@onet.pl>
2023-07-14 20:33:52 +0200

    add codepen shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -63,6 +63,22 @@ module.exports = function(eleventyConfig) {
         });
     });
 
+    // ref: https://syntackle.live/blog/eleventy-shortcode-for-embedding-codepen-ZyslIPzCHpJo3kkPwu2U/
+    eleventyConfig.addLiquidShortcode('codepen', function (url) {
+
+        const url_array = url.split('/');
+
+        const profile_url_array = url_array.filter((string, index) => {
+            return (index < (url_array.length - 2)) ? true : false
+        })
+
+        const username = profile_url_array[profile_url_array.length - 1];
+        const user_profile = profile_url_array.join("/");
+        const data_slug_hash = url_array[url_array.length - 1];
+
+        return `<p class="codepen" data-height="600" data-default-tab="result" data-slug-hash="${data_slug_hash}" data-user="${username}" style="height: 571px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;"><span><a href="${url}">See the pen</a> (<a href="${user_profile}">@${username}</a>) on <a href="https://codepen.io">CodePen</a>.</span></p><script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>`;
+    });
+
     eleventyConfig.addFilter('md', function(content) {
         return md.render(content).trim().replace(/(^<p>)|(<\/p>$)/g, '');
     });
