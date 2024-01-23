2bb92f38c8f3fed6371f924895fd29f3c85d149c
Jakub Jankiewicz <jcubic@onet.pl>
2024-01-23 18:29:52 +0100

    add web mentions

--- .eleventy.js
+++ .eleventy.js
@@ -226,6 +226,44 @@ module.exports = function(eleventyConfig) {
         return content; // no change done.
     });
 
+    eleventyConfig.addFilter('webmentionsByUrl', function(webmentions, url) {
+        const allowedTypes = ['in-reply-to', 'like-of', 'repost-of'];
+
+        const data = {
+            'like-of': [],
+            'repost-of': [],
+            'in-reply-to': [],
+        };
+
+        const hasRequiredFields = entry => {
+            const { author, published, content } = entry
+            return author.name && published && content
+        };
+
+        const filtered = webmentions
+              .filter(entry => entry['wm-target'] === `https://rknight.me${url}`)
+              .filter(entry => allowedTypes.includes(entry['wm-property']));
+
+        filtered.forEach(m => {
+            if (data[m['wm-property']]) {
+                const isReply = m['wm-property'] === 'in-reply-to';
+                const isValidReply = isReply && hasRequiredFields(m);
+                if (isReply) {
+                    if (isValidReply) {
+                        m.sanitized = sanitizeHTML(m.content.html);
+                        data[m['wm-property']].unshift(m);
+                    }
+
+                    return;
+                }
+
+                data[m['wm-property']].unshift(m);
+            }
+        });
+
+        return data;
+    });
+
     const url = dev ? 'http://localhost:8080' : 'https://jakub.jankiewicz.org';
 
     eleventyConfig.addGlobalData('site', {
